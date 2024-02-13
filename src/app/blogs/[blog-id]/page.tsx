bb025027449f4be3b820c9962866b4285f05db68
Katsukiniwa <katsukiniwa0217@gmail.com>
2024-02-13 11:06:59 +0900

    chore: edit page text

--- src/app/blogs/[blog-id]/page.tsx
+++ src/app/blogs/[blog-id]/page.tsx
@@ -2,7 +2,7 @@ import { wrapper } from "./page.css";
 export default function BlogPage() {
   return (
     <div className={wrapper}>
-      <h1>[id] Blog Page</h1>
+      <h1>[blog-id] Blog Page</h1>
     </div>
   );
 }
